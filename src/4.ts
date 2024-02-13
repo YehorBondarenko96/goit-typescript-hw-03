feba21702667b7f8e0399a85d7234783aac5605c
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-02-13 16:38:21 +0200

    4.0.0-13.02.24

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key{
+    private signature:number
+
+    constructor(){
+        this.signature = Math.random();
+    }
+
+    getSignature():number {
+        return this.signature
+    }
+};
+
+class Person{
+    constructor(private key: Key){}
+
+    getKey():Key{
+        return this.key
+    }
+};
+
+abstract class House{
+    protected door: boolean = false;
+    protected key: Key;
+    public tenants: Person[] = [];
+
+    constructor(door: boolean, key: Key){
+        this.door = door;
+        this.key = key;
+    }
+
+    comeIn(person: Person): void{
+        if(this.door){
+            this.tenants.push(person)
+        }
+    };
+
+    abstract openDoor(key: Key): void;
+};
+
+class MyHouse extends House{
+    constructor(key: Key){
+        super(false, key)
+    }
+
+    openDoor(key: Key): void{
+        if(key.getSignature() === this.key.getSignature()){
+            this.door = true;
+        }
+    }
+};
+
 const key = new Key();
 
 const house = new MyHouse(key);
